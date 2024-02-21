9d2da4a8cd2eec06f86a53e3e38f4e3adcce8a06
Evans <evanstats808@gmail.com>
2024-02-21 21:28:24 +0300

    frontend with react

--- src/App.js
+++ src/App.js
@@ -15,13 +15,51 @@ import config from './config.json';
 
 function App() {
   const [account , setAccount] = useState(null)
+  const [provider , setProvider] = useState(null)
+  const [escrow , setEscrow] = useState(null)
+  const [homes , setHomes] = useState([])
+  const [toggle, setToggle] = useState(null)
+  const[home, sethome] = useState(null)
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const accounts = await window.ethereum.request( { method: 'eth_requestAccounts'})
-    setAccount(accounts[0])
-    console.log(accounts[0])
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    // const escrowAddress = config[network.chainId].escrow.address 
+    // const realEstateAddress= config[network.chainId].realEstate.address
+     // console.log(realEstateAddress)
+    const realEstateContract = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+    //const totalSupply = await realEstateContract.totalsupply()
+    //console.log(totalSupply)
+    const homes = []
+    for (var i = 1; i<= 3; i++) {
+      const uri = await realEstateContract.tokenURI(i)
+      const response = await fetch(uri)
+      const metadata = await response.json()
+      homes.push(metadata)
+
+    } 
+    setHomes(homes)
+    
+
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+    setEscrow(escrow)
+    
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request( {method: 'eth_requestAccounts'} )
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
   }
+  
+  const togglePop = (home) => {
+    sethome(home)
+    toggle ? setToggle(false) : setToggle(true)
+
+  }
+ 
 
   useEffect(() => {
     loadBlockchainData()
@@ -30,11 +68,45 @@ function App() {
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
+
       <div className='cards__section'>
 
-        <h3>Welcome to Millow!</h3>
+        <h3>Homes for you</h3>
+        <hr />
+
+        <div class name = 'cards'>
+          {homes.map((home, index) => (
+            <div className='card' key = {index} onClick={() => togglePop(home)}>
+              <div className='card__image'>
+                <img src = {home.image} alt='Home' />
+              
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value}</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong>  bds I
+                  <strong>{home.attributes[3].value}</strong>  ba I
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+
+              </div>
+
+            </div>
+
+          
+
+
+          ))}
+
+        </div>  
 
       </div>
+      {toggle && (
+        <Home home={home} provider={provider} account={account} escrow={escrow} togglePop={togglePop}/>
+      )}
 
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,162 @@ import { useEffect, useState } from 'react';
 
 import close from '../assets/close.svg';
 
-const Home = ({ home, provider, escrow, togglePop }) => {
+const Home = ({ home, account, provider, escrow, togglePop }) => {
+    const [hasBought, setHasBought] = useState(false)
+    const [haslended, setHasLended] = useState(false)
+    const [hasInspected, setHasInspected] = useState(false)
+    const [hasSold, setHasSold] = useState(false)
 
+    const [buyer, setBuyer] = useState(null)
+    const [ lender, setLender] = useState(null)
+    const [inspector, setInspector] = useState(null)
+    const [seller, setSeller] = useState(null)
+
+    const [owner, setOwner] = useState(null)
+
+    const fetchDetails = async () => {
+    
+        const buyer = await escrow.buyer(home.id)
+        setBuyer(buyer)
+        const hasBought = await escrow.approval(home.id,buyer)
+        setHasBought(hasBought)
+        
+
+        const seller = await escrow.seller()
+        setSeller(seller)
+        const hasSold = await escrow.approval(home.id,seller)
+        setHasSold(hasSold)
+
+        const lender = await escrow.lender()
+        setLender(lender)
+        const haslended = await escrow.approval(home.id,lender)
+        setHasLended(haslended)
+
+        const inspector = await escrow.inspector()
+        setInspector(inspector)
+        const hasInspected = await escrow.passedInspection(home.id)
+        setHasInspected(hasInspected)
+
+    }
+    const fetchOwner = async () => {
+        if (await escrow.islisted(home.id)) return
+        const owner = await escrow.buyer(home.id)
+        setOwner(owner)
+    }
+
+    const buyHandler = async () => {
+        const escrowAmount = await escrow.escrowAmount(home.id)
+        const signer = await provider.getSigner()
+
+        let transaction = await escrow.connect(signer).depositEarnest(home.id, { value: escrowAmount })
+        await transaction.wait()
+        setHasBought(true)
+    }   
+
+    const inspectHandler = async () => {
+        const signer = await provider.getSigner()
+
+        const transaction = await escrow.connect(signer).passedInspection(home.id, true)
+        await transaction.wait()
+
+    }
+    const letHandler = async () => {
+        const signer = await provider.getSigner()
+
+        const transaction = await connect(signer).approveSale(home.id)
+        await transaction.await()
+
+        const lendAmount = (await escrow.purchasePrice(home.id) - await escrow.escrowAmount(home.id))
+        await signer.sendTransaction({ to: escrow.address, value: lendAmount.toString(), gaslimit: 60000})
+
+        setHasLended(true)
+    }   
+
+    const sellHandler = async () => {
+
+    }
+    useEffect(() => {
+        fetchDetails()
+        fetchOwner()
+    }, [hasSold])
+  
     return (
         <div className="home">
+            <div className='home__details'>
+                <div className='home__image'>
+                    <img src={home.image} alt = "Home" />
+
+                </div>
+                <div className='home__overview'>
+                    <h1>{home.name}</h1>
+                    <p>
+                        <strong>{home.attributes[2].value}</strong> bds I
+                        <strong>{home.attributes[3].value}</strong> ba I
+                        <strong>{home.attributes[4].value}</strong> sqft
+                    </p>
+                    <p>{home.address}</p>
+                    <h2>{home.attributes[0].value} ETH</h2>
+                    
+                    {owner ? (
+                        <div className = 'home__owned'>
+                            Owned by {owner.slice(0, 6) + '.....' + owner.slice(38, 42)}
+                        </div>
+
+                    ) : (
+                        <div>
+                            {(account ===inspector) ? (
+                                <button className='home__buy' onClick={inspectHandler} disabled={hasInspected}>
+                                    Approve Inspection
+                                </button>
+                            ) : (account === lender) ? (
+                                <button className='home__buy' onClick={letHandler} disabled={haslended}>
+                                    Approve & lend
+                                </button>
+                            ) : (account === seller) ? (
+                                <button className='home__buy' onClick={sellHandler} disabled = {hasSold}>
+                                    Approve & Sell
+                                </button>
+                            ) : (
+                                <button className='home__sell' onClick={buyHandler} disabled={hasBought}>
+                                    Buy
+                                </button>
+                            )}
+                        
+                        </div>
+
+                    )}
+                    
+                    <div>
+                        <button className='home__buy'>
+                            buy
+                        </button>
+                        <button className='home__contact'>
+                            Contact agent
+                        </button>
+
+                    </div>
+                    <hr></hr>
+                   
+                    <p>
+                    {home.description}
+                    </p>
+                    <hr></hr>
+                    <h2>facts and features</h2>
+                    <ul>
+                        {home.attributes.map((attribute, index) => (
+                            <li key={index}><strong>{attribute.trait_type}</strong> : {attribute.value}</li>
+
+                        ))}
+                    
+                    </ul>
+
+                </div>
+
+
+            </div>
+            <button onClick={togglePop} className='home__close'>
+                <img src={close} alt="close"></img>
+            </button>
         </div>
     );
 }
